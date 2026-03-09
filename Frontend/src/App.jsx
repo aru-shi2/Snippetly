0df69fca8a8936cf8a065eb42e5272707473824e
aru-shi2 <arushimishra2929@gmail.com>
2026-03-09 21:52:50 +0530

    hero section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-amber-700'>djf</div>
+    <div className='px-10'>
+      <div className="header flex justify-between">
+        <div className="name pt-5 text0lg font-bold">
+          Snippetly
+        </div>
+        <div className='theme pt-5'>
+          <button>Light</button>
+        </div>
+      </div>
+      <div className="content flex gap-30 absolute top-[30%]">
+        <div className="l space-y-15">
+          <div className="text space-y-8">
+            <h1 className='font-bold text-4xl'>Turn Your Code Into Beautiful Snippets</h1>
+          <p className='text-xl'>Create stunning, shareable images of your code in seconds. Perfect for social media, blogs, and documentation.</p>
+          </div>
+          <button className='bg-yellow-200 px-5 h-15 rounded-2xl text-lg'>Get started</button>
+        </div>
+
+        <div className="r w-[70%]">
+          <img className='w-[90%] rounded-2xl' src="/public/Pic/Snap.png" alt="" />
+        </div>
+      </div>
+    </div>
     </>
   )
 }
