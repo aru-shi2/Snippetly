70c6f87f52bd711583170a74e3b566737b8be072
aru-shi2 <arushimishra2929@gmail.com>
2026-03-21 19:45:38 +0530

    add toggle button

--- Frontend/src/Components/Snippets.jsx
+++ Frontend/src/Components/Snippets.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Togglebtn from './Togglebtn'
 
 const Snippets = () => {
   return (
     <div>
-      
+      <div className="topp">
+        <div className="theme">
+            <label htmlFor="Colour">Color:</label>
+            <select name="Colour" id="Colour">
+                <option value="one">One</option>
+                <option value="two">Two</option>
+                <option value="custom">Custom</option>
+            </select>
+        </div>
+        <div className="bgd">
+          <Togglebtn/>
+        </div>
+
+      </div>
     </div>
   )
 }
