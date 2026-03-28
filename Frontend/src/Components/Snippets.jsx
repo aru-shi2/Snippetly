c3ea93b150b30257d9cab7c29128ecd0bde4f78d
aru-shi2 <arushimishra2929@gmail.com>
2026-03-28 07:46:57 -0600

    add options

--- Frontend/src/Components/Snippets.jsx
+++ Frontend/src/Components/Snippets.jsx
@@ -4,9 +4,9 @@ import Togglebtn from './Togglebtn'
 const Snippets = () => {
   return (
     <div>
-      <div className="topp">
+      <div className="topp flex gap-5">
         <div className="theme">
-            <label htmlFor="Colour">Color:</label>
+            <label htmlFor="Colour">Theme</label>
             <select name="Colour" id="Colour">
                 <option value="one">One</option>
                 <option value="two">Two</option>
@@ -14,9 +14,70 @@ const Snippets = () => {
             </select>
         </div>
         <div className="bgd">
+          <label htmlFor='bg'>Background</label>
           <Togglebtn/>
         </div>
 
+        <div className="theme">
+          <label htmlFor="theme">Card Theme</label>
+          <Togglebtn/>
+        </div>
+
+        <div className="no">
+          <label htmlFor="line">Line Numbers</label>
+          <Togglebtn/>
+        </div>
+
+        <div className="font">
+          <label htmlFor="fntsize">Font size</label>
+          <select name="fntsize" id="fntsize">
+            <option value="o">Small</option>
+            <option value="m">Medium</option>
+            <option value="l">Large</option>
+          </select>
+        </div>
+
+        <div className="round">
+          <label htmlFor="radius">Rounded Corners</label>
+          <Togglebtn/>
+        </div>
+
+        <div className="padding">
+          <label htmlFor="space">Padding</label>
+          <select name="space" id="space">
+            <option value="n">None</option>
+            <option value="n">Small</option>
+            <option value="m">Medium</option>
+            <option value="l">Large</option>
+          </select>
+        </div>
+
+        <div className="lng">
+          <label htmlFor="lang">Language</label>
+          <select name="Language" id="Language">
+            <option value="a">Auto Detect (Default)</option>
+            <option value="j">JavaScript</option>
+            <option value="">C</option>
+            <option value="">C++</option>
+            <option value="">Python</option>
+            <option value="">Java</option>
+            <option value="">HTML</option>
+            <option value="">CSS</option>
+          </select>
+        </div>
+
+        <div className="s">
+        <label htmlFor="st">Style</label>
+        <select name="st" id="st">
+          <option value="">Mac</option>
+          <option value="">VS Code</option>
+          <option value="">Minimal (No Header)</option>
+        </select>
+        </div>
+      </div>
+
+      <div className="txt">
+        <textarea name="" id="" placeholder></textarea>
       </div>
     </div>
   )
